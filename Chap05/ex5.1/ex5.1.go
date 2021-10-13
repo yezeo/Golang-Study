3106520ec912f982d0779c0f77756e634863372e
seondal <sseondal@gmail.com>
2021-10-13 12:38:45 +0900

    chapter05

---
+++ Chap05/ex5.1/ex5.1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 10
+	var f float64 = 32799438743.8287
+
+	fmt.Print("a:", a, "b:", b)
+	fmt.Println("a:", a, "b:", b, "f", f)
+	fmt.Printf("a: %d, b: %d f:%f\n", a, b, f)
+}
---
+++ Chap05/ex5.1/go.mod
@@ -0,0 +1,3 @@
+module Chap05/ex5.1
+
+go 1.17
