b8f24a48813f169e35e8d5ea83ffff1fa80057b1
haayun <kng06325@gmail.com>
2022-02-01 23:10:57 +0900

    [12] Ch30 - 31

---
+++ Ch30/ex30.1/ex30.1.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"sort"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Student struct {
+	Id    int
+	Name  string
+	Age   int
+	Score int
+}
+
+var students map[int]Student
+var lastId int
+
+func MakeWebHandler() http.Handler {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/students", GetStudentListHandler).Methods("GET")
+
+	mux.HandleFunc("/students/{id:[0-9]+}", GetStudentHandler).Methods("GET")
+	mux.HandleFunc("/students", PostStudentHandler).Methods("POST")
+	mux.HandleFunc("/students/{id:[0-9]+}", DeleteStudentHandler).Methods("DELETE")
+
+	students = make(map[int]Student)
+	students[1] = Student{1, "aaa", 16, 87}
+	students[2] = Student{2, "bbb", 18, 98}
+	lastId = 2
+
+	return mux
+}
+
+type Students []Student
+
+func (s Students) Len() int {
+	return len(s)
+}
+func (s Students) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s Students) Less(i, j int) bool {
+	return s[i].Id < s[j].Id
+}
+
+func GetStudentListHandler(w http.ResponseWriter, r *http.Request) {
+	list := make(Students, 0)
+	for _, student := range students {
+		list = append(list, student)
+	}
+
+	sort.Sort(list)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(list)
+}
+
+func GetStudentHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	student, ok := students[id]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(student)
+}
+
+func PostStudentHandler(w http.ResponseWriter, r *http.Request) {
+	var student Student
+	err := json.NewDecoder(r.Body).Decode(&student)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	lastId++
+	student.Id = lastId
+	students[lastId] = student
+	w.WriteHeader(http.StatusCreated)
+}
+
+func DeleteStudentHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	_, ok := students[id]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	delete(students, id)
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.ListenAndServe(":3000", MakeWebHandler())
+}
---
+++ Ch31/ex31.1/go.mod
@@ -0,0 +1,18 @@
+module ex31.1
+
+go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.7.0
+	github.com/unrolled/render v1.4.1
+	github.com/urfave/negroni v1.0.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
